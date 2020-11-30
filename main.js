2fe36cf2ec7b978a0e8e130e7f5b831cc9515ab1
Leila Sosa González <leisosa93@gmail.com>
2020-11-30 12:30:58 -0300

    filtra playlist

---
+++ main.js
@@ -0,0 +1,55 @@
+const playlist1 = document.getElementById("playlist1");
+const playlist2 = document.getElementById("playlist2");
+const playlist3 = document.getElementById("playlist3");
+const tracksContainer = document.getElementById("tracks-container");
+const playlistContainer = document.getElementById("playlist-container");
+let filtro = "";
+
+window.onload = () => {
+  obtenerPlaylist();
+  playlistContainer.addEventListener("click", filtrarPlaylist);
+};
+
+const filtrarPlaylist = (e) => {
+  e.preventDefault();
+  filtro = e.target.id;
+  obtenerPlaylist(filtro);
+};
+
+const obtenerPlaylist = (filtro) => {
+  try {
+    fetch("./playlist.json")
+      .then((res) => res.json())
+      .then((data) => mostrarPlaylist(data.playlist, filtro));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const limpiarHTML = () => {
+  while (tracksContainer.firstChild) {
+    tracksContainer.removeChild(tracksContainer.firstChild);
+  }
+};
+
+const mostrarPlaylist = (listado, filtro) => {
+  limpiarHTML();
+
+  const scripting = (track) => {
+    tracksContainer.innerHTML += `
+            <div class="track-container">
+                <i class="fas fa-play"></i>
+                <h6 class="mt-1"></h6>
+                <img src="./img/${track.titulo}.jpg" alt="album">
+                <h5 class="mt-1">${track.titulo}</h5>
+                <h6 class="mt-1">${track.artista}</h6>
+            </div>`;
+  };
+
+  listado.map((track) => {
+    const { categoria } = track;
+    if (filtro === track.categoria) {
+      scripting(track);
+    }
+  });
+};
